38385b02e3196440c254341d5f06daae48e5255c
KTR200 <j122241@g.kogakuin.jp>
2025-03-14 00:22:07 +0900

    rollback db

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,3 @@
-import { KVNamespace, DurableObjectNamespace, D1Database } from "@cloudflare/workers-types";
-
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
@@ -7,13 +5,6 @@ declare global {
     interface Locals {
       user: import("$lib/server/auth").SessionValidationResult["user"];
       session: import("$lib/server/auth").SessionValidationResult["session"];
-    };
-    interface Platform {
-      env?: {
-        YOUR_KV_NAMESPACE: KVNamespace;
-        YOUR_DURABLE_OBJECT_NAMESPACE: DurableObjectNamespace;
-        DB: D1Database,
-      };
     }
   }
 }
