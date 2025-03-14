4085a52bb458a0b7c55ec467497c48846c9b62b7
Ryuki Terasawa <j122241@g.kogakuin.jp>
2025-03-14 10:08:49 +0900

    manual

--- src/routes/dev/+page.svelte
+++ src/routes/dev/+page.svelte
@@ -5,19 +5,28 @@ import type { PageServerData } from "./$types";
 let { data }: { data: PageServerData } = $props();
 </script>
 
-<h1 class="text-4xl">開発者向けページ</h1>
-{#if !data.user}
-    <div class="m-2">
-        {data.user}
-        <p>開発者向けのログインはこちらから</p>
-        <form method="post" action="/login?/login_github" use:enhance>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >GitHubでログイン</button
-            >
-        </form>
-    </div>
-{:else}
-    <div>
-        <a href="/dev/dashboard">あなたのダッシュボード</a>
-    </div>
-{/if}
+<div class="container mx-auto p-5">
+    <h1 class="text-3xl mb-3">開発者向けマニュアル</h1>
+    {#if !data.user}
+        <div class="mb-2">
+            {data.user}
+            <p>開発者向けのログインはこちらから</p>
+            <form method="post" action="/login?/login_github" use:enhance>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >GitHubでログイン</button
+                >
+            </form>
+        </div>
+    {:else}
+        <div class="mb-3">
+            <p>マニュアルは只今準備中です｡ダッシュボードへは下記のリンクからお入りください｡</p>
+            <p>
+                ダッシュボードでは自身の制作した拡張機能(ゴースト)の確認と登録ができます｡登録の際は､ダッシュボードの登録フォームに拡張機能のGitHubリポジトリのURLを貼り付けてください｡
+            </p>
+            <p>ご不便をおかけして申し訳ありません｡</p>
+        </div>
+        <div>
+            <a class="text-blue-500 hover:text-blue-700" href="/dev/dashboard">あなたのダッシュボード</a>
+        </div>
+    {/if}
+</div>
