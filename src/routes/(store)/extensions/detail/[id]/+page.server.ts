ce33540ef9e9cbe7d37d69d545b7de8ebb554e2a
Ryuki Terasawa <j122241@g.kogakuin.jp>
2025-03-15 01:14:32 +0900

    detail

--- src/routes/(store)/extensions/detail/[id]/+page.server.ts
+++ src/routes/(store)/extensions/detail/[id]/+page.server.ts
@@ -14,7 +14,8 @@ export const load: PageServerLoad = async ({ params }) => {
   const [result] = await db
     .select({
       id: table.extension.id, name: table.extension.name, description: table.extension.description,
-      icon_url: table.extension.icon_url, category_id: table.category.id, category_name: table.category.name,
+      icon_url: table.extension.icon_url, zipball_url: table.extension.zipball_url,
+      category_id: table.category.id, category_name: table.category.name,
       version: table.extension.version,
       userId: table.user.id, username: table.user.username, githubAvatarUrl: table.user.githubAvatarUrl,
       createdAt: table.extension.created_at, updatedAt: table.extension.updated_at,
